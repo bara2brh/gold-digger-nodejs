654a0b951da56f12b0ed2b3eeba056c0a3ee0847
bhashlamoon <bara1brh@gmail.com>
2026-05-07 15:45:15 +0300

    serve 404 error page

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -4,10 +4,19 @@ import path from 'node:path';
 import fs from 'node:fs/promises'
 
 export async function serveStatic(req, res, baseDir) {
-    const filePath = path.join(baseDir, req.url == '/' ? 'index.html' : req.url.slice(1))
-    const ext = path.extname(filePath)
-    const contentType = getContentType(ext)
-    const payload = await fs.readFile(filePath, 'utf-8')
-    sendResponse(res, 201, contentType, payload)
+    try {
+        const filePath = path.join(baseDir, req.url == '/' ? 'index.html' : req.url)
+        const ext = path.extname(filePath)
+        const contentType = getContentType(ext)
+        const payload = await fs.readFile(filePath)
+        sendResponse(res, 201, contentType, payload)
+    } catch (err) {
+        const filePath = path.join(baseDir, '404.html')
+        const ext = path.extname(filePath)
+        const contentType = getContentType(ext)
+        const payload = await fs.readFile(filePath)
+        sendResponse(res, 404, contentType, payload)
+    }
 
-}
+
+} 
